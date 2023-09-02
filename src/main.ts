50cf16b5025941328a54a88132a73f1346da4dc9
Draghici Rares-Valentin <draghici.rares212@gmail.com>
2023-09-02 21:14:04 +0300

    add buy button | implement toolbar route fragments (wip) | implement seo service (wip) | module refactors | add configs for urls

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {NestMiddleware, ValidationPipe} from "@nestjs/common";
-import AdminJS from "adminjs";
-import * as AdminJSMongoose from '@adminjs/mongoose'
-import * as mongoose from "mongoose";
+import { ValidationPipe } from '@nestjs/common';
+import AdminJS from 'adminjs';
+import * as AdminJSMongoose from '@adminjs/mongoose';
+import * as mongoose from 'mongoose';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log', 'debug', 'verbose']
-  });
-  app.useGlobalPipes(new ValidationPipe({transform: true}));
+    const app = await NestFactory.create(AppModule, {
+        logger: ['error', 'warn', 'log', 'debug', 'verbose']
+    });
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  app.setGlobalPrefix('printnuts-api');
+    app.setGlobalPrefix('printhaus-api');
 
-  AdminJS.registerAdapter({
-    Resource: AdminJSMongoose.Resource,
-    Database: AdminJSMongoose.Database
-  });
+    AdminJS.registerAdapter({
+        Resource: AdminJSMongoose.Resource,
+        Database: AdminJSMongoose.Database
+    });
 
-  await app.listen(3000);
+    await app.listen(3000);
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 
-  mongoose.set('debug', true);
+    mongoose.set('debug', true);
 }
 
 bootstrap();
